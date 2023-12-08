54c0ebdc659ef1bcba67eaccd45c57d57dc3ddd1
viniciusabr <152444385+viniciusabr@users.noreply.github.com>
2023-12-08 19:30:50 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function teste(){
     var nome = document.getElementById('txt-nome').value;
     var senha = document.getElementById('txt-pass').value;
     var erro = document.getElementById('js');
-    if (nome == 'vinicius' && senha == '123'){
+    if (nome == 'higor' && senha == '123'){
         window.location.assign("inicio.html")
     }else{
         erro.innerHTML = '<p>Usuário ou senha incorretos!</p>'
